dce9c10014ce6170525042968d5543122ecbb8c7
yeti1851 <github@isiu.net>
2015-05-05 22:35:34 -0600

    Added comments and tabs

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Gods White Board:</title>
+<title>Gods White Board :: Evergreen, CO</title>
 <script type="text/javascript" src="include/jquery-1.11.1.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative' rel='stylesheet' type='text/css'>
 
 <link rel="stylesheet" type="text/css" href="include/bootstrap.min.css"/>
 <link rel="stylesheet" type="text/css" href="include/bootstrap-theme.min.css"/>
+
+/* codes
 <style type="text/css">
 body{ background-image:url(images/Board.png);
 }
@@ -19,10 +21,8 @@ h1,h2,h3,h4,h5,h6,p{color:#FFF;
 .board{  font-family:'Shadows Into Light Two', cursive; font-weight:400; border: solid 15px #FCFEFD;
 padding:60px 20px;
 }
-
-
-
 </style>
+
 </head>
 
 <body>
@@ -46,23 +46,21 @@ padding:60px 20px;
         <div class="board" align="center">
         
        
-            <?
+            <?php // add code to display latest text 
 			include("include/server.php");
 			mysqli_select_db ($con, $DataBaseName);
 			$sql = "SELECT * FROM $wightboard WHERE status = '1' ORDER BY date DESC LIMIT 1";
              $check  = mysqli_query($con, $sql )or die(mysqli_error($con));
-							
-						while($info1 = mysqli_fetch_array( $check )){
-							
-				echo '<h2 class="fonttype2">'.$info1['note1'].'</h2>';			
-						}
+			  while($info1 = mysqli_fetch_array( $check )) {
+			   echo '<h2 class="fonttype2">' . $info1['note1'] . '</h2>';			
+			  }
 			?>
             
-             </div>
-             <div align="right" style="padding:20px;">
-             <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count"></div>
-             </div>
         </div>
+        <div align="right" style="padding:20px;">
+ <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count"></div>
+ </div>
+</div>
 </div>
 
 </body>
